f676cd1b3a170bf06d48f533d8080e7c3c75b190
ALLAIRE Benjamin <benjamin.allaire76@gmail.com>
2018-12-17 14:03:00 +0100

    fin quest sport de la 1ere partie

--- 1ere version/sport/appsport.js
+++ 1ere version/sport/appsport.js
@@ -14,8 +14,23 @@ var quizz =[
           {"question":'A quoi correspond le maillot à pois du tour de France ? ',
           "reponse":['Meilleur jeune','Meilleur grimpeur', 'Plus grande combativité']},
 
-          {"question":'A quoi correspond le maillot à pois du tour de France ? ',
-          "reponse":['Meilleur jeune','Meilleur grimpeur', 'Plus grande combativité']},
+          {"question":'Quel est le record, en nba, du plus grand nombre de panier à 3 point
+         marqué sur un match? ',
+          "reponse":['10','12 ','14']},
+
+          {"question":'Comment s’appelle la personne qui porte les clubs de golf durant une
+         partie ? ',
+          "reponse":['Un caddie','Un clubeur','Un golfeur']},
+
+          {"question":'Au baseball, comment s’appelle un coup qui permet de faire un tour de
+         circuit ?',
+          "reponse":['Home run','Hit run','Bat run']},
+
+          {"question":'Combien de temps a durée la plus longue partie d’échec de l’histoire ?',
+          "reponse":['3h43','20h15','8h12']},
+
+          {"question":'n quelle année Yannick Noah a t-il gagné Roland Garros ?',
+          "reponse":['1978',' 1983','1997']},
         	];
 
 
