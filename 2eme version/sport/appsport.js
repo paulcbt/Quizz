4c67d6860aebec79750da1f04d5cb18ba4e53849
ALLAIRE Benjamin <benjamin.allaire76@gmail.com>
2018-12-17 14:53:55 +0100

    notification vrai faux pages sport

---
+++ 2eme version/sport/appsport.js
@@ -0,0 +1,19 @@
+var recupv = document.getElementById('vraie');
+
+recupv.addEventListener("click", changev , false);
+
+
+function changev(){
+	alert("bonne réponse :) ");
+}
+
+
+
+var recupv = document.getElementById('faux');
+
+recupv.addEventListener("click", changef , false);
+
+
+function changef(){
+	alert("mauvaise réponse :/");
+}
---
+++ appsport.js
@@ -0,0 +1,8 @@
+var recupv = document.getElementById('vraie');
+
+recupv.addEventListener("click", changev , false);
+
+
+function changev(){
+	alert("bonne réponse score: ");
+}
